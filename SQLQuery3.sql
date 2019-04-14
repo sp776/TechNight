949ded35f45518b2d14c588aed73696e4ed8ec33
sp776 <47477836+sp776@users.noreply.github.com>
2019-04-14 00:20:19 -0400

    Add files via upload

---
+++ SQLQuery3.sql
@@ -0,0 +1,19 @@
+INSERT INTO Organizer1
+VALUES ('Best Friends Lifesaving Center', 'Animals', 40.721531, -74.004112),
+('Animal Haven', 'Animals', 40.716351, -74.001244),
+('Sean Casey Animal Rescue', 'Animals', 40.697440, -73.979440),
+('ASPCA Adoption Center', 'Animals', 40.780183, -73.945627),
+('NYC New Beginning Animal Rescue', 'Animals', 40.780183, -73.945627),
+('PAWS NY', 'Animals', 40.747246, -73.991706),
+('Bobbi & the Strays', 'Animals', 40.7021239, -73.8804598),
+('Literacy Partners Inc.', 'Education', 40.707686, -73.979440),
+('AHRC New York City', 'Education', 40.70752, -74.00736),
+('Working in Support of Education', 'Education', 40.759218, -73.966905),
+('Learning Leaders', 'Education', 40.707686, -74.007666),
+('Education Through Music', 'Education', 40.65283, -73.938414),
+('The Kabbalah Centre', 'Education', 40.714751, -73.962209),
+('Make the Road New York', 'Education', 40.698416, -73.915796),
+('AHRC', 'Education', 40.676586, -73.998476),
+('Friendship Circle of Brooklyn', 'Education', 40.669003, -73.941962),
+('Girls on the Run', 'Education', 40.700738, -73.987335);
+SELECT * FROM Organizer1;
